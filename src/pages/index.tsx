d35d762b6e99d72040f1b2bde3ecd6e4e3371e79
felipe <felipe.coutinho.psl@gmail.com>
2023-05-22 21:33:02 -0300

    fist commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+
 import Header from '@/components/Home/Header/Header'
 import HomePage from '@/components/Home/Home'
 
 
-
-const inter = Inter({ subsets: ['latin'] })
+//ok
 
 export default function Home() {
   return (
